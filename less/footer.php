3caa769d4b8d6b8a60becaea3b6df22fb7da10dc
Norio <umanari145@gmail.com>
2017-10-08 10:43:03 +0900

    lessの簡単なサンプル

---
+++ less/footer.php
@@ -0,0 +1,11 @@
+<footer>
+  <div id="fotter_block">footer左</div>
+  <ul id="footer_menu">
+    footerみぎ
+    <li id="menu_list" class="no_style"></li>
+    <li id="menu_list" class="no_style"></li>
+    <li id="menu_list" class="no_style"></li>
+    <li id="menu_list" class="no_style"></li>
+    <li id="menu_list" class="no_style"></li>
+  </ul>
+</footer>
---
+++ less/header.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>サンプル</title>
+<link rel="stylesheet" href="css/top.css">
+</head>
+<body>
+<header>
+  <h1 id="logo">aaa</h1>
+  <ul id="menu_block">
+    <li id="each_list"><a href="#">test</a></li>
+    <li id="each_list"><a href="#">test</a></li>
+    <li id="each_list"><a href="#">test</a></li>
+    <li id="each_list"><a href="#">test</a></li>
+    <li id="each_list"><a href="#">test</a></li>
+  </ul>
+</header>
