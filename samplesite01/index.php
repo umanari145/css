7b05215397d7f335a2b8c9962014dcf35d927721
Norio <umanari145@gmail.com>
2017-10-15 11:43:13 +0900

    checkboxのカスタマイズ

--- samplesite01/index.php
+++ samplesite01/index.php
@@ -1,12 +1,10 @@
 <?php
 include_once "header.php";
-?>
 <section>
 <div class="EntryPanel">
 	<div class="EntryPanel__sub">
 		<img class="EntryPanel__thumb" src="">
 	</div>
-
 	<div class="EntryPanel__main">
 		<h3 class="EntryPanel__head">冬のコーヒーギフト</h3>
 		<div class="EntryPanel__body">大切なあの方にありがとうを届けたい。心も体も温まるギフト。プレゼントキャンペーン実施中。!</div>
