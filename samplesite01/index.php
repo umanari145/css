caa0344f0af6a7d57445ad96265ac4cd43d71711
Norio <umanari145@gmail.com>
2018-01-13 20:51:19 +0900

    flexサイトの修正

--- samplesite01/index.php
+++ samplesite01/index.php
@@ -1,31 +1,30 @@
 <?php include_once "header.php";?>
-<section>
-<div class="EntryPanel">
-	<div class="EntryPanel__sub">
-		<img class="EntryPanel__thumb" src="">
-	</div>
-	<div class="EntryPanel__main">
-		<h3 class="EntryPanel__head">冬のコーヒーギフト</h3>
-		<div class="EntryPanel__body">大切なあの方にありがとうを届けたい。心も体も温まるギフト。プレゼントキャンペーン実施中。!</div>
-		<div class="EntryPanel__footer">
-			<a href="#">read more</a>
+<section class="Item_block">
+	<div class="EntryPanel">
+		<div class="EntryPanel__sub">
+			<img class="EntryPanel__thumb" src="./images/top/entry1.jpg">
+		</div>
+		<div class="EntryPanel__main">
+			<h3 class="EntryPanel__head">冬のコーヒーギフト</h3>
+			<div class="EntryPanel__body">大切なあの方にありがとうを届けたい。心も体も温まるギフト。プレゼントキャンペーン実施中。!</div>
+			<div class="EntryPanel__footer">
+				<a href="#">read more</a>
+			</div>
 		</div>
 	</div>
-</div>
 
-<div class="EntryPanel">
-	<div class="EntryPanel__sub">
-		<img class="EntryPanel__thumb" src="">
-	</div>
+	<div class="EntryPanel">
+		<div class="EntryPanel__sub">
+			<img class="EntryPanel__thumb" src="">
+		</div>
 
-	<div class="EntryPanel__main">
-		<h3 class="EntryPanel__head">あの人気店コラボメニュー</h3>
-		<div class="EntryPanel__body">今話題の人気店、Macron Thingのパンケーキを使ったコラボ商品のセットメニューを販売中</div>
-		<div class="EntryPanel__footer">
-			<a href="#">read more</a>
+		<div class="EntryPanel__main">
+			<h3 class="EntryPanel__head">あの人気店コラボメニュー</h3>
+			<div class="EntryPanel__body">今話題の人気店、Macron Thingのパンケーキを使ったコラボ商品のセットメニューを販売中</div>
+			<div class="EntryPanel__footer">
+				<a href="#">read more</a>
+			</div>
 		</div>
 	</div>
-</div>
-
 
 </section>
